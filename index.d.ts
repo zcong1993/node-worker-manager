f9b2f4eaf8d6b4a6ba5f27fd9ee09fb42fd6ce28
zcong1993 <1005997919@qq.com>
2018-06-25 10:28:30 +0800

    chore(type): tweak type defination

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
 import { EventEmitter } from 'events'
 
-export interface Opts {
-  workerPath: string,
-  nums?: number,
-  recreate?: boolean,
-  handler?: (msg: any) => void,
-  workerOpts?: object
-}
-
-export class WorkerManager extends EventEmitter {
+declare class WorkerManager extends EventEmitter {
   workerNums: number
-  constructor(opts: Opts)
+  constructor(opts: WorkerManager.Opts)
   dispatch(msg: any)
 }
 
-export default WorkerManager
+declare namespace WorkerManager {
+  interface Opts {
+    workerPath: string,
+    nums?: number,
+    recreate?: boolean,
+    handler?: (msg: any) => void,
+    workerOpts?: object
+  }
+}
+
+export = WorkerManager
